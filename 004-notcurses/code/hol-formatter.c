13b137d809a9026aed78c30170d095d237e1db97
nick black <dankamongmen@gmail.com>
2020-02-16 01:20:28 -0500

    fix bug in hol-formatter.c

--- 004-notcurses/code/hol-formatter.c
+++ 004-notcurses/code/hol-formatter.c
@@ -8,7 +8,7 @@ int main(void){
   int c, ret = 0;
   if(n){
     while(!ret && (c = getchar()) != EOF){
-      if(*b == c){
+      if(isalpha(*b) && tolower(*b) == c){
         ++b;
       }else{
         if(b > blue){
