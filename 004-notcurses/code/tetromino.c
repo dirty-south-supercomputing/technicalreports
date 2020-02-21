a47c4943e33de47f84ba3a481bc7b5a7aaa53838
nick black <dankamongmen@gmail.com>
2020-02-20 22:42:23 -0500

    004: draw tetriminos

--- 004-notcurses/code/tetromino.c
+++ 004-notcurses/code/tetromino.c
@@ -1,10 +1,11 @@
 #include <locale.h>
+#include <unistd.h>
 #include <notcurses/notcurses.h>
 
 // "North-facing" tetrimino forms (the form in which they are released from the
 // top) are expressed in terms of two rows having between two and four columns.
 // We map each game column to four columns and each game column to two rows.
-// strlen(texture) / 2 / 4 -> game columns per game row for the tetrimino.
+// strlen(texture) / COLS_PER_GCOL -> columns per (g)row for the tetrimino.
 static const int COLS_PER_GCOL = 4;
 static const int ROWS_PER_GROW = 2;
 
@@ -14,13 +15,13 @@ static const struct tetrimino {
   const char* texture;
 } tetriminos[] = {
      { 'O', 0xfada5e, "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" },
-     { 'I', 0x34ccff, "                                                "
-                      "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+     { 'I', 0x34ccff, "                                "
+                      "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
   }, { 'T', 0x800080, "    TTTT        TTTT    "
                       "TTTTTTTTTTTTTTTTTTTTTTTT",
   }, { 'L', 0xff6900, "        LLLL        LLLL"
                       "LLLLLLLLLLLLLLLLLLLLLLLL"
-  }, { 'J', 0x00008b, "LLLL        LLLL        "
+  }, { 'J', 0x004bbb, "LLLL        LLLL        "
                       "LLLLLLLLLLLLLLLLLLLLLLLL"
   }, { 'S', 0x32cd32, "    SSSSSSSS    SSSSSSSS"
                       "SSSSSSSS    SSSSSSSS    "
@@ -31,10 +32,33 @@ static const struct tetrimino {
 
 static struct ncplane*
 tetrimino_plane(struct notcurses* nc, int tidx){
+  if(tidx < 0 || tidx >= sizeof(tetriminos) / sizeof(*tetriminos)){
+    return NULL;
+  }
   const struct tetrimino* t = &tetriminos[tidx];
-  struct ncplane* n = ncplane_new(nc, 2 * ROWS_PER_GROW,
-                                  strlen(t->texture) / 8, 0, 0, NULL);
-  // FIXME draw it
+  const size_t cols = strlen(t->texture) / (2 * ROWS_PER_GROW);
+  struct ncplane* n = ncplane_new(nc, 2 * ROWS_PER_GROW, cols, 0, 0, NULL);
+  if(n){
+    ncplane_set_fg(n, t->color);
+    int y = 0;
+    size_t x = 0;
+    for(size_t i = 0 ; i < strlen(t->texture) ; ++i){
+      if(x == 0){
+        if(ncplane_cursor_move_yx(n, y, x)){
+          ncplane_destroy(n);
+          return NULL;
+        }
+      }
+      if(ncplane_putsimple(n, t->texture[i]) < 0){
+        ncplane_destroy(n);
+        return NULL;
+      }
+      if(++x == cols){
+        ++y;
+        x = 0;
+      }
+    }
+  }
   return n;
 }
 
@@ -48,6 +72,18 @@ int main(void){
   if((nc = notcurses_init(&nopts, stdout)) == NULL){
     return EXIT_FAILURE;
   }
-  // FIXME
-  return notcurses_stop(nc) ? EXIT_FAILURE : EXIT_SUCCESS;
+  bool failed = true;
+  while(true){
+    struct ncplane* n = tetrimino_plane(nc, random() % 7);
+    if(!n){
+      break;
+    }
+    if(notcurses_render(nc)){
+      ncplane_destroy(n);
+      break;
+    }
+    sleep(1);
+    ncplane_destroy(n);
+  }
+  return (notcurses_stop(nc) || failed) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
