985fc6294c19ac052931f4674ce149184651a5b7
nick black <dankamongmen@gmail.com>
2020-02-25 13:22:34 -0500

    urk

--- 004-notcurses/code/tetrimino-databox.h
+++ 004-notcurses/code/tetrimino-databox.h
@@ -1,8 +1,7 @@
 // "North-facing" tetrimino forms (the form in which they are released from the
 // top) are expressed in terms of two rows having between two and four columns.
 // We map each game column to four columns and each game column to two rows.
-// Each byte of the texture maps to one 4x4 component block.
-
+// Each byte of the texture maps to one 4x4 component block (and wastes 7 bits).
 static const struct tetrimino {
   unsigned color;
   const char* texture;
