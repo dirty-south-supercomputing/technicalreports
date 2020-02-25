6651b79240b700b12e42a877726a93b9dcb14292
nick black <dankamongmen@gmail.com>
2020-02-25 08:06:01 -0500

    jre-default

--- 004-notcurses/code/tetrimino-draw.h
+++ 004-notcurses/code/tetrimino-draw.h
@@ -1,11 +1,11 @@
-static int draw_tetriminos(struct notcurses* nc, struct ncplane** minos, int dimy, int dimx){
-  if( (minos[0] = tetrimino_plane(nc, 0, dimy / 3, dimx / 3)) ){
-    if( (minos[1] = tetrimino_plane(nc, 1, dimy / 3, dimx * 2 / 3)) ){
-      if( (minos[2] = tetrimino_plane(nc, 2, dimy / 2, dimx / 4)) ){
-        if( (minos[3] = tetrimino_plane(nc, 3, dimy / 2, dimx / 2)) ){
-          if( (minos[4] = tetrimino_plane(nc, 4, dimy / 2, dimx * 3 / 4)) ){
-            if( (minos[5] = tetrimino_plane(nc, 5, dimy * 2 / 3, dimx / 3)) ){
-              if( (minos[6] = tetrimino_plane(nc, 6, dimy * 2 / 3, dimx * 2 / 3)) ){
+static int draw_tetriminos(struct notcurses* nc, struct ncplane** minos, int y, int x){
+  if( (minos[0] = tetrimino_plane(nc, 0, y / 3, x / 3)) ){
+    if( (minos[1] = tetrimino_plane(nc, 1, y / 3, x * 2 / 3)) ){
+      if( (minos[2] = tetrimino_plane(nc, 2, y / 2, x / 4)) ){
+        if( (minos[3] = tetrimino_plane(nc, 3, y / 2, x / 2)) ){
+          if( (minos[4] = tetrimino_plane(nc, 4, y / 2, x * 3 / 4)) ){
+            if( (minos[5] = tetrimino_plane(nc, 5, y * 2 / 3, x / 3)) ){
+              if( (minos[6] = tetrimino_plane(nc, 6, y * 2 / 3, x * 2 / 3)) ){
                 return 0;
               } ncplane_destroy(minos[5]);
             } ncplane_destroy(minos[4]);
@@ -16,4 +16,3 @@ static int draw_tetriminos(struct notcurses* nc, struct ncplane** minos, int dim
   }
   return -1;
 }
-
