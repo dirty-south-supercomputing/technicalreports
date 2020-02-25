7b832288dc4bfda818b1873c7a5f882025b44776
nick black <dankamongmen@gmail.com>
2020-02-25 12:37:24 -0500

    fix that up

--- 004-notcurses/code/tetrimino-displayutf8.h
+++ 004-notcurses/code/tetrimino-displayutf8.h
@@ -5,17 +5,16 @@ tetrimino_plane(struct notcurses* nc, int tidx, int yoff, int xoff){
   const struct tetrimino* t = &tetriminos[tidx];
   const size_t cols = strlen(t->texture);
   struct ncplane* n = ncplane_new(nc, 2 * ROWS_PER_GROW, cols, yoff, xoff, NULL);
-  uint64_t channels;
-  channels_set_bg_alpha(&channels, CELL_ALPHA_TRANSPARENT);
-  channels_set_fg_alpha(&channels, CELL_ALPHA_TRANSPARENT);
   if(n){
+    uint64_t channels = 0;
+    channels_set_bg_alpha(&channels, CELL_ALPHA_TRANSPARENT);
+    channels_set_fg_alpha(&channels, CELL_ALPHA_TRANSPARENT);
     ncplane_set_fg(n, t->color);
     ncplane_set_base(n, channels, 0, "");
     size_t y = 0, x = 0;
     for(size_t i = 0 ; i < strlen(t->texture) ; ++i){
       if(t->texture[i] == '*'){
-        ncplane_cursor_move_yx(n, y, x);
-        if(ncplane_putstr(n, "██") < 0){
+        if(ncplane_putstr_yx(n, y, x, "██") < 0){
           ncplane_destroy(n);
           return NULL;
         }
