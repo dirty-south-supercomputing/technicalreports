e5553381c929c6f1a164fd7d99eb75c8c75cd4c4
nick black <dankamongmen@gmail.com>
2020-03-05 10:21:15 -0500

    tetrimino_plane(): only need 2 rows duh

--- 004-notcurses/code/tetrimino-displayutf8.h
+++ 004-notcurses/code/tetrimino-displayutf8.h
@@ -3,7 +3,7 @@
 static struct ncplane* tetrimino_plane(struct notcurses* nc, int tidx, int yoff, int xoff){
   const struct tetrimino* t = &tetriminos[tidx];
   const size_t cols = strlen(t->texture);
-  struct ncplane* n = ncplane_new(nc, 2 * ROWS_PER_GROW, cols, yoff, xoff, NULL);
+  struct ncplane* n = ncplane_new(nc, ROWS_PER_GROW, cols, yoff, xoff, NULL);
   if(n){
     uint64_t channels = 0;
     channels_set_bg_alpha(&channels, CELL_ALPHA_TRANSPARENT);
