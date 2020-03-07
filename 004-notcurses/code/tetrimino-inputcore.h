52fdfb9a75fae39ef4b4f0f55c3361767426fc54
nick black <dankamongmen@gmail.com>
2020-03-07 15:31:46 -0500

    don't be a buffoon

--- 004-notcurses/code/tetrimino-inputcore.h
+++ 004-notcurses/code/tetrimino-inputcore.h
@@ -13,17 +13,17 @@ static bool handle_input(struct notcurses* nc, struct ncplane** ps, int dimy, in
     *p = (*p + (key == ' ' ? (TETRIMINO_COUNT - 1) : 1)) % TETRIMINO_COUNT;
     ncplane_yx(ps[*p], y, x);
   }else if(key == '('){ // rotate counterclockwise
-    return ncplane_rotate_ccw(ps[*p]) ? true : false;
+    return ncplane_rotate_ccw(ps[*p]);
   }else if(key == ')'){ // rotate clockwise
-    return ncplane_rotate_cw(ps[*p]) ? true : false;
+    return ncplane_rotate_cw(ps[*p]);
   }else if(key == NCKEY_LEFT || key == 'h'){
-    return ncplane_move_yx(ps[*p], *y, --*x < 0 ? *x = 0 : *x) ? true : false;
+    return ncplane_move_yx(ps[*p], *y, --*x < 0 ? *x = 0 : *x);
   }else if(key == NCKEY_RIGHT || key == 'l'){
-    return ncplane_move_yx(ps[*p], *y, *x = (*x + ncplane_dim_x(ps[*p]) > dimx ? *x : *x + 1)) ? true : false;
+    return ncplane_move_yx(ps[*p], *y, *x = (*x + ncplane_dim_x(ps[*p]) > dimx ? *x : *x + 1));
   }else if(key == NCKEY_UP || key == 'k'){
-    return ncplane_move_yx(ps[*p], --*y < 0 ? *y = 0 : *y, *x) ? true : false;
+    return ncplane_move_yx(ps[*p], --*y < 0 ? *y = 0 : *y, *x);
   }else if(key == NCKEY_DOWN || key == 'j'){
-    return ncplane_move_yx(ps[*p], ++*y + 2 > dimy ? *y = dimy - 2 : *y, *x) ? true : false;
+    return ncplane_move_yx(ps[*p], ++*y + 2 > dimy ? *y = dimy - 2 : *y, *x);
   }else if(nckey_mouse_p(key)){
     return handle_mouse_event(nc, &ni);
   }
