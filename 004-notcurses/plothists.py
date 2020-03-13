b6a79a5b736fc565093643934305933f9899e677
nick black <dankamongmen@gmail.com>
2020-03-13 12:00:56 -0400

    graphs motherfuckers

--- 004-notcurses/plothists.py
+++ 004-notcurses/plothists.py
@@ -7,43 +7,48 @@ import matplotlib.pyplot as plt
 
 sns.set(style="darkgrid")
 
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(20,8))
 
-total = pd.DataFrame()
-bases = ['xfce4-terminal-80x52', 'alacritty-80x55', 'kitty-80x55']
-for b in bases:
-    for i in range(1,6):
-        print(b + str(i))
-        xterm=pd.read_json('data/' + b + '-' + str(i))
-        print(xterm)
-        print(xterm.keys())
-        demo = xterm['notcurses-demo']
-        print(demo.keys())
-        runs = demo['runs']
-        print(runs.keys())
-        print(runs.values())
-        nruns = json_normalize(runs);
-        nruns.insert(0, "Term", b.split('-')[0])
-        total = pd.concat([total, nruns], ignore_index=True)
-print(total)
-print('*********************************')
-for k in total.keys():
-    tokes = k.split('.')
-    if len(tokes) == 2 and tokes[1] != "ns":
-        total.drop(columns=k, inplace=True)
-print(total)
-print('*********************************')
-print(total.keys())
-mtotal = pd.melt(total,
-                 id_vars=["Term"],
-                 var_name="Demo")
-print(mtotal)
-mtot = mtotal.astype({'value':'int64'}).sort_values("value", ascending=True)
-print(mtot)
+suffixes = ['ns', 'bytes']
+for suf in suffixes:
+    total = pd.DataFrame()
+    bases = ['xfce4-terminal-80x52', 'alacritty-80x52', 'kitty-80x52']
+    for b in bases:
+        for i in range(1,6):
+            xterm=pd.read_json('data/' + b + '-' + str(i))
+            print(xterm)
+            print(xterm.keys())
+            demo = xterm['notcurses-demo']
+            print(demo.keys())
+            runs = demo['runs']
+            print(runs.keys())
+            print(runs.values())
+            nruns = json_normalize(runs);
+            nruns.insert(0, "Term", b.split('-')[0])
+            total = pd.concat([total, nruns], ignore_index=True)
+    print(total)
+    print('*********************************')
+    for k in total.keys():
+        tokes = k.split('.')
+        if len(tokes) == 2 and tokes[1] != suf:
+            total.drop(columns=k, inplace=True)
+        elif len(tokes) == 2:
+            total.rename(columns={k:tokes[0]}, inplace=True)
+    print(total)
+    print('*********************************')
+    print(total.keys())
+    mtotal = pd.melt(total,
+                    id_vars=["Term"],
+                    var_name="Demo")
+    print(mtotal)
+    mtot = mtotal.astype({'value':'int64'}).sort_values("value", ascending=True)
+    print(mtot)
 
-fig, lax = plt.subplots()
-lax.set(yscale="log", ylim=[1000000, 40000000000])
-ax = sns.swarmplot(x="Demo", y='value', data=mtot,  hue="Term", orient='v', dodge=True, ax=lax)
-plt.legend(bbox_to_anchor=(1, 1), loc=2)
-plt.show()
-#plt.savefig('swarmplot.png', dpi=350)
+    #fig, lax = plt.subplots()
+    #lax.set(yscale="log", ylim=[1000000, 40000000000])
+    ax = sns.swarmplot(x="Demo", y='value', data=mtot,  hue="Term", orient='v', dodge=True)#, ax=lax)
+    plt.legend(bbox_to_anchor=(1, 1), loc=2)
+    plt.xlabel('Demo')
+    plt.ylabel('Nanoseconds')
+    plt.show()
+    plt.savefig('swarmplot-' + suf + '.png', dpi=350)
