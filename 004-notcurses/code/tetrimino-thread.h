3a00a38dd31ce2f199bb8a57b19d36f3ad07fa87
nick black <dankamongmen@gmail.com>
2020-03-14 21:59:19 -0400

    respect the gutter

--- 004-notcurses/code/tetrimino-thread.h
+++ 004-notcurses/code/tetrimino-thread.h
@@ -25,9 +25,10 @@ static void* rotator_thread(void* vmarsh){
     pthread_mutex_lock(&marsh->lock);
     const int radius = dimy < dimx ? dimy / 3 : dimx / 3;
     for(int i = 0 ; i < TETRIMINO_COUNT ; ++i){
-      const bool lift = strspn(tetriminos[i].texture, " ") == strlen(tetriminos[i].texture) / 2;
+      const size_t len = strlen(tetriminos[i].texture);
+      const bool lift = strspn(tetriminos[i].texture, " ") == len / 2;
       int y = centy + radius * sin(sector * i + ssector * iterations) - (ROWS_PER_GROW + lift * 2);
-      int x = centx + aspect * radius * cos(sector * i + ssector * iterations) - strlen(tetriminos[i].texture) / (2 * COLS_PER_GCOL);
+      int x = centx + aspect * radius * cos(sector * i + ssector * iterations) - len / (2 * COLS_PER_GCOL);
       if(ncplane_move_yx(marsh->minos[i], y, x)){
         pthread_mutex_unlock(&marsh->lock);
         return NULL;
