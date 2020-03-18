d5510ab2d6bb1e4c96d07d818e34e4e6a9e34df3
nick black <dankamongmen@gmail.com>
2020-03-18 18:52:39 -0400

    kitty data incoming

--- 004-notcurses/plotrun.py
+++ 004-notcurses/plotrun.py
@@ -18,7 +18,8 @@ for suf in suffixes:
     ttl = pd.DataFrame()
     bases = ['xfce4-vte-52-', 'xfce4-vte2-52-', 'xfce4-vte3-52-',
              'xterm-bitmap-52-', 'xterm-bitmap2-52-',
-             'alacritty-52-', 'alacritty-2-52-', 'alacritty-3-52-']
+             'alacritty-52-', 'alacritty-2-52-', 'alacritty-3-52-',
+             'kitty-1-52-']
     widths = [] # widths
     times = [] # nanosecond counts
     terms = [] # terminals
