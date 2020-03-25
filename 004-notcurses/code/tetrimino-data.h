c90613ba796c0f1faea1e8514cf02bba19c43bd0
nick black <dankamongmen@gmail.com>
2020-03-25 10:11:47 -0400

    some close editing

--- 004-notcurses/code/tetrimino-data.h
+++ 004-notcurses/code/tetrimino-data.h
@@ -1,7 +1,6 @@
-// "North-facing" tetrimino forms (the form in which they are released from the
-// top) are expressed in terms of two rows having between two and four columns.
-// We map each game column to four columns and each game column to two rows.
-// strlen(texture) / COLS_PER_GCOL -> columns per (g)row for the tetrimino.
+// "North-facing" tetrimino forms (the form in which they are released from the top) are expressed in terms of
+// two rows having between two and four columns. We map each game column to four columns and each game column
+// to two rows. strlen(texture) / COLS_PER_GCOL -> columns per (g)row for the tetrimino.
 static const int ROWS_PER_GROW = 2;
 static const int COLS_PER_GCOL = 4;
 
