3fb995ba16bfcb7df1f49a5320f9c75bf78e1829
nick black <dankamongmen@gmail.com>
2020-03-25 22:13:08 -0400

    import new tetris codes

--- 004-notcurses/code-tetris/stuck.h
+++ 004-notcurses/code-tetris/stuck.h
@@ -6,14 +6,29 @@ bool PieceStuck() {
     int y, x;
     curpiece_->get_dim(&y, &x);
     while(x--){
+      ncpp::Cell piecec;
+      if(curpiece_->get_at(y - 1, x, &piecec) < 0){
+        throw TetrisNotcursesErr("get_at()");
+      }
+      if(!piecec.get().gcluster || piecec.get().gcluster == ' '){
+        continue;
+      }
+      const char* egc = curpiece_->get_extended_gcluster(piecec);
+      if(strcmp(egc, "▄") && strcmp(egc, "█")){
+        continue;
+      }
       int cmpy = y, cmpx = x; // need game area coordinates via translation
       curpiece_->translate(*board_, &cmpy, &cmpx);
       ncpp::Cell c;
       if(board_->get_at(cmpy, cmpx, &c) < 0){
         throw TetrisNotcursesErr("get_at()");
       }
-      if(c.get().gcluster && c.get().gcluster != ' '){
-        return true;
+      if(c.get().gcluster){
+        if(c.get().gcluster != ' '){
+          if(strcmp(board_->get_extended_gcluster(c), "▄")){
+            return true;
+          }
+        }
       }
     }
   }
