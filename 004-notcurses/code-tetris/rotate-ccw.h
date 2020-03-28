fe8aaa5b4af4d2dd228ef878c2204d74ca0d206a
nick black <dankamongmen@gmail.com>
2020-03-28 01:02:07 -0400

    more text

--- 004-notcurses/code-tetris/rotate-ccw.h
+++ 004-notcurses/code-tetris/rotate-ccw.h
@@ -14,20 +14,3 @@ void RotateCcw() {
     throw TetrisNotcursesErr("render()");
   }
 }
-
-void RotateCw() {
-  const std::lock_guard<std::mutex> lock(mtx_);
-  int y, x;
-  if(!PrepForMove(&y, &x)){
-    return;
-  }
-  if(!curpiece_->rotate_cw()){
-    throw TetrisNotcursesErr("rotate_cw()");
-  }
-  if(InvalidMove() && !curpiece_->rotate_ccw()){
-    throw TetrisNotcursesErr("rotate_ccw()");
-  }
-  if(!nc_.render()){
-    throw TetrisNotcursesErr("render()");
-  }
-}
---
+++ 004-notcurses/code-tetris/rotate-cw.h
@@ -0,0 +1,16 @@
+void RotateCw() {
+  const std::lock_guard<std::mutex> lock(mtx_);
+  int y, x;
+  if(!PrepForMove(&y, &x)){
+    return;
+  }
+  if(!curpiece_->rotate_cw()){
+    throw TetrisNotcursesErr("rotate_cw()");
+  }
+  if(InvalidMove() && !curpiece_->rotate_ccw()){
+    throw TetrisNotcursesErr("rotate_ccw()");
+  }
+  if(!nc_.render()){
+    throw TetrisNotcursesErr("render()");
+  }
+}
