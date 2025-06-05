c597b4ad0023c577b3ae24fc76b21e084c3d54b4
nick black <dankamongmen@gmail.com>
2025-06-05 05:07:01 -0400

    rearrange chapter 1

--- 006-pgo/types.c
+++ 006-pgo/types.c
@@ -356,7 +356,7 @@ static void
 defensive_relations_latex(const typing* t){
   printf("\\begingroup\n");
   printf("\\setlength{\\tabcolsep}{1pt}");
-  printf("\\begin{longtable}{cC{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}C{1em}}\n");
+  printf("\\begin{longtable}{ccccccccccccccccccc}\n");
   for(int k = 0 ; k < TYPECOUNT ; ++k){
     printf("& \\includegraphics[width=1em]{images/%s.png}", tnames[k]);
   }
