100c1dd8a0e1e368caff33fa4f775d072675932a
nick black <dankamongmen@gmail.com>
2025-06-14 05:34:49 -0400

    don't sort by type in form lists

--- 006-pgo/forms.c
+++ 006-pgo/forms.c
@@ -5,31 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*static void
-print_cpms(void){
-  printf("cpms:\n");
-  for(int i = 1 ; i < 100 ; ++i){
-    printf("%2d %f\n", i, cpm(i));
-  }
-}*/
-
-static void
-print_species(const species* s){
-  if(s->t2 == TYPECOUNT){
-    printf("%u %u %u %u %s %s\n", s->idx, s->atk, s->def, s->sta, TNames[s->t1], s->name);
-  }else{
-    printf("%u %u %u %u %s+%s %s\n", s->idx, s->atk, s->def, s->sta,
-            TNames[s->t1], TNames[s->t2], s->name);
-  }
-  stats* s2500 = find_optimal_set(s, 2500, 0);
-  while(s2500){
-    stats* tmp = s2500->next;
-    // print s2500
-    free(s2500);
-    s2500 = tmp;
-  }
-}
-
 int main(int argc, char **argv){
   if(argc != 2){
     fprintf(stderr, "usage: %s mega|primal|crowned|dynamax|gigantamax\n", argv[0]);
@@ -56,15 +31,10 @@ int main(int argc, char **argv){
     fprintf(stderr, "usage: %s mega|primal|crowned|dynamax|gigantamax\n", argv[0]);
     return EXIT_FAILURE;
   }
-  // we want to retain the order of typings here, so do both ways
-  for(int t = TYPESTART ; t < static_cast<int>(TYPECOUNT) ; ++t){
-    // we always want the pure typing first
-    filter_by_types(t, t, dex, count);
-    for(int t2 = TYPESTART ; t2 < static_cast<int>(TYPECOUNT) ; ++t2){
-      if(t != t2){
-        filter_by_types(t, t2, dex, count);
-      }
-    }
+  // for forms, we just want them alphabetical, not by type
+  // for now we use dex entry FIXME
+  for(unsigned u = 0 ; u < count ; ++u){
+    print_species_latex(&dex[u]);
   }
   return EXIT_SUCCESS;
 }
