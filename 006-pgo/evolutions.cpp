8bfb909e11987c364d9aece9687c417773baab00
nick black <dankamongmen@gmail.com>
2025-06-19 03:48:45 -0400

    evolution tables

--- 006-pgo/evolutions.cpp
+++ 006-pgo/evolutions.cpp
@@ -1,10 +1,56 @@
 #include "pgotypes.cpp"
 
 static int
+print_evolutions(int depth){
+  for(auto &dex : sdexen){
+    for(unsigned u = 0 ; u < dex.dcount ; ++u){
+      const species *s = &dex.dex[u];
+      if(s->from){
+        const species *s1 = lookup_species(s->from);
+        if(s1 == NULL){
+          return -1;
+        }
+        if(depth == 2){
+          if(s1->from){
+            continue;
+          }
+          // FIXME ensure nothing links to s
+        }else if(depth == 3){
+          if(!s1->from){
+            continue;
+          }
+          const species *s2 = lookup_species(s1->from);
+          if(s2 == NULL){
+            return -1;
+          }
+          if(s2->from){
+            continue;
+          }
+          escape_string(s2->name.c_str());
+          puts(" & ");
+        }else{
+          return -1;
+        }
+        escape_string(s1->name.c_str());
+        puts(" & ");
+        escape_string(s->name.c_str());
+        puts("\\\\");
+      }
+    }
+  }
+  return 0;
+}
+
+static int
 twostagetable(void){
   puts("\\begin{center}");
   puts("\\footnotesize");
-  puts("\\begin{longtable}{ll}");
+  puts("\\begin{longtable}{lcl}");
+  puts("Stage 1 & Requirements & Stage 2\\\\");
+  puts("\\Midrule");
+  if(print_evolutions(2)){
+    return -1;
+  }
   puts("\\caption{Two-stage evolutionary paths}");
   puts("\\label{table:twostage}");
   puts("\\end{longtable}");
@@ -16,7 +62,12 @@ static int
 threestagetable(void){
   puts("\\begin{center}");
   puts("\\footnotesize");
-  puts("\\begin{longtable}{ll}");
+  puts("\\begin{longtable}{lclcl}");
+  puts("Stage 1 & Requirements & Stage 2 & Requirements & Stage 3\\\\");
+  puts("\\Midrule");
+  if(print_evolutions(3)){
+    return -1;
+  }
   puts("\\caption{Three-stage evolutionary paths}");
   puts("\\label{table:threestage}");
   puts("\\end{longtable}");
