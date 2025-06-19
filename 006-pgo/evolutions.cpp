aa67767743e34b9ffb82dc6ba084351fa257ffb3
nick black <dankamongmen@gmail.com>
2025-06-19 04:00:28 -0400

    evolutionary tables working

--- 006-pgo/evolutions.cpp
+++ 006-pgo/evolutions.cpp
@@ -2,40 +2,39 @@
 
 static int
 print_evolutions(int depth){
-  for(auto &dex : sdexen){
-    for(unsigned u = 0 ; u < dex.dcount ; ++u){
-      const species *s = &dex.dex[u];
-      if(s->from){
-        const species *s1 = lookup_species(s->from);
-        if(s1 == NULL){
-          return -1;
-        }
-        if(depth == 2){
-          if(s1->from){
-            continue;
-          }
-          // FIXME ensure nothing links to s
-        }else if(depth == 3){
-          if(!s1->from){
-            continue;
-          }
-          const species *s2 = lookup_species(s1->from);
-          if(s2 == NULL){
-            return -1;
-          }
-          if(s2->from){
-            continue;
-          }
-          escape_string(s2->name.c_str());
-          puts(" & ");
-        }else{
-          return -1;
-        }
-        escape_string(s1->name.c_str());
-        puts(" & ");
-        escape_string(s->name.c_str());
-        puts("\\\\");
+  // we don't want to search the other sdexen; this is permanent evolutions only
+  for(unsigned u = 0 ; u < SPECIESCOUNT ; ++u){
+    const species *s = &sdex[u];
+    if(s->from){
+      const species *s1 = lookup_species(s->from);
+      if(s1 == NULL){
+        return -1;
       }
+      if(depth == 2){
+        if(s1->from){
+          continue;
+        }
+        // FIXME ensure nothing links to s
+      }else if(depth == 3){
+        if(!s1->from){
+          continue;
+        }
+        const species *s2 = lookup_species(s1->from);
+        if(s2 == NULL){
+          return -1;
+        }
+        if(s2->from){
+          continue;
+        }
+        escape_string(s2->name.c_str());
+        puts(" & FIXME & ");
+      }else{
+        return -1;
+      }
+      escape_string(s1->name.c_str());
+      puts(" & FIXME & ");
+      escape_string(s->name.c_str());
+      puts("\\\\");
     }
   }
   return 0;
@@ -45,7 +44,7 @@ static int
 twostagetable(void){
   puts("\\begin{center}");
   puts("\\footnotesize");
-  puts("\\begin{longtable}{lcl}");
+  puts("\\begin{longtable}{lll}");
   puts("Stage 1 & Requirements & Stage 2\\\\");
   puts("\\Midrule");
   if(print_evolutions(2)){
@@ -62,7 +61,7 @@ static int
 threestagetable(void){
   puts("\\begin{center}");
   puts("\\footnotesize");
-  puts("\\begin{longtable}{lclcl}");
+  puts("\\begin{longtable}{lllll}");
   puts("Stage 1 & Requirements & Stage 2 & Requirements & Stage 3\\\\");
   puts("\\Midrule");
   if(print_evolutions(3)){
