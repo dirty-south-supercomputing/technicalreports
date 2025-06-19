493df0d148131f47713f852207eca16314aa549f
nick black <dankamongmen@gmail.com>
2025-06-19 01:24:59 -0400

    italicize pokemon without STAB

--- 006-pgo/users.cpp
+++ 006-pgo/users.cpp
@@ -10,9 +10,12 @@ print_attack_users_sdex(const attack *a, const species *dex, unsigned dcount,
         if(*printed){
           printf(", ");
         }
+        if(!has_stab_p(s, *sa)){
+          printf("\\textit{");
+        }
         escape_string(s->name.c_str());
         if(!has_stab_p(s, *sa)){
-          printf("⁻");
+          printf("}");
         }
         *printed = true;
       }
@@ -25,7 +28,7 @@ print_attack_users(const attack *a){
   bool printed = false;
   printf("\\begin{tcolorbox}[enhanced,title=");
   escape_string(a->name);
-  printf(",colbacktitle=%s,before title={",
+  printf(",colbacktitle=%s,fonttitle=\\bfseries,before title={",
           a->type == TYPECOUNT ? "White" : TNames[a->type]);
   print_type(a->type);
   printf(" ");
