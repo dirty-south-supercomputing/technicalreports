2d75a161b6a8bcc99d349151f51ef59ce10fc60d
nick black <dankamongmen@gmail.com>
2025-06-19 01:53:31 -0400

    count populated dual typings

--- 006-pgo/populations.cpp
+++ 006-pgo/populations.cpp
@@ -4,11 +4,12 @@
 // have a presence
 int main(void){
   puts("\\begin{table}[ht]");
-  puts("\\begin{centering}");
-  puts("\\begin{tabular}{lrrr}");
-  puts("Type & Pop & Dualtypes & Pop\\\\\n");
+  puts("\\begin{center}");
+  puts("\\begin{tabular}{lrrrr}");
+  puts("Type & Pop & Dualtypes & Pop & Total\\\\\n");
   puts("\\Midrule\n");
   for(int t = 0 ; t < TYPECOUNT ; ++t){
+    bool duals[TYPECOUNT] = {};
     unsigned pop = 0;
     unsigned dts = 0;
     unsigned dtpop = 0;
@@ -18,17 +19,27 @@ int main(void){
           const species *s = &sd.dex[i];
           if(s->t1 == t && (s->t2 == t || s->t2 == TYPECOUNT)){
             ++pop;
-          }else if(s->t1 == t || s->t2 == t){
-            // FIXME handle dts
+          }else if(s->t1 == t){
+            if(!duals[s->t2]){
+              ++dts;
+            }
+            duals[s->t2] = true;
+            ++dtpop;
+          }else if(s->t2 == t){
+            if(!duals[s->t1]){
+              ++dts;
+            }
+            duals[s->t1] = true;
             ++dtpop;
           }
         }
       }
     }
-    printf("%s & %u & %u & %u\\\\\n", TNames[t], pop, dts, dtpop);
+    printf("%s & %u & %u & %u & %u\\\\\n",
+        TNames[t], pop, dts, dtpop, pop + dtpop);
   }
   puts("\\end{tabular}");
-  puts("\\end{centering}");
+  puts("\\end{center}");
   puts("\\caption{Population of types}");
   puts("\\label{table:populations}");
   puts("\\end{table}");
