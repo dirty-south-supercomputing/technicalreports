2de5f7c5c92bd1fff630ca7769d6e341d0865f44
nick black <dankamongmen@gmail.com>
2025-06-27 22:37:57 -0400

    cut some space between species boxen

--- 006-pgo/pgotypes.cpp
+++ 006-pgo/pgotypes.cpp
@@ -12760,7 +12760,7 @@ print_previous_species(const species *s){
 }
 
 void print_species_latex(const species* s, bool overzoom, bool vfill){
-  printf("\\begin{tcolorbox}[enhanced,top=0pt,bottom=0pt,boxsep=0pt,middle=0pt,title=\\#%04u ", s->idx);
+  printf("\\begin{tcolorbox}[enhanced,before skip=0pt,after skip=0pt,top=0pt,bottom=0pt,boxsep=0pt,middle=0pt,title=\\#%04u ", s->idx);
   escape_string(s->name.c_str());
   printf(",title style={left color=%s,right color=%s},fonttitle=\\bfseries,after title={",
           TNames[s->t1], s->t2 == TYPECOUNT ? TNames[s->t1] : TNames[s->t2]);
