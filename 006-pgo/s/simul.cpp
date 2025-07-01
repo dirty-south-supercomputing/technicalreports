54148323928f403b90179fe3a35f47b5891c8c30
nick black <dankamongmen@gmail.com>
2025-07-01 06:39:38 -0400

    extract tophalf

--- 006-pgo/s/simul.cpp
+++ 006-pgo/s/simul.cpp
@@ -183,43 +183,6 @@ bottomhalf(simulstate *s, results *r, pgo_move_e m0, pgo_move_e m1){
   tophalf(s, r); // no one got knocked out; recurse to next turn
 }
 
-// each turn has a top half and bottom halves.
-// in the top half we determine which choice-pairs can be performed.
-// in the bottom halves, we simulate a choice-pair.
-static void
-tophalf(const simulstate *s, results *r){
-  //printf("tophalf turn %u %u %u %u\n", s->turn, r->wins[0], r->wins[1], r->ties);
-  bool m0[MOVEMAX] = {};
-  bool m1[MOVEMAX] = {};
-  sift_choices(s, m0, 0);
-  sift_choices(s, m1, 1);
-  for(int c0 = 0 ; c0 < MOVEMAX ; ++c0){
-    if(m0[c0]){
-      for(int c1 = 0 ; c1 < MOVEMAX ; ++c1){
-        if(m1[c1]){
-          // if c0 is a shielded move, it's only relevant if c1 is a charged
-          // move, and vice versa
-          if(shielded_move_p(static_cast<pgo_move_e>(c0))){
-            if(!charged_move_p(static_cast<pgo_move_e>(c1))){
-              continue;
-            }
-          }
-          if(shielded_move_p(static_cast<pgo_move_e>(c1))){
-            if(!charged_move_p(static_cast<pgo_move_e>(c0))){
-              continue;
-            }
-          }
-          auto cs = std::make_unique<simulstate>();
-          *cs.get() = *s;
-          ++cs->turn;
-          //printf("calling bottomhalf with moves %d %d hp %d %d turn %u %u\n", c1, c2, cs->hp[0], cs->hp[1], s->turn, cs->turn);
-          bottomhalf(cs.get(), r, static_cast<pgo_move_e>(c0), static_cast<pgo_move_e>(c1));
-        }
-      }
-    }
-  }
-}
-
 static void
 simul(simulstate *s, results *r){
   s->turns[0] = s->turns[1] = 0u;
@@ -269,6 +232,7 @@ int main(int argc, char** argv){
   simulstate sstate;
   --argc;
   ++argv;
+  // FIXME need read three per team
   if(lex_pmon(&pmons[0][0], &sstate.hp[0][0], &argc, &argv)){
     usage(argv0);
   }
---
+++ 006-pgo/s/top.h
@@ -0,0 +1,37 @@
+// each turn has a top half and bottom halves.
+// in the top half we determine which choice-pairs can be performed.
+// in the bottom halves, we simulate a choice-pair.
+static void
+tophalf(const simulstate *s, results *r){
+  //printf("tophalf turn %u %u %u %u\n", s->turn, r->wins[0], r->wins[1], r->ties);
+  bool m0[MOVEMAX] = {};
+  bool m1[MOVEMAX] = {};
+  sift_choices(s, m0, 0);
+  sift_choices(s, m1, 1);
+  for(int c0 = 0 ; c0 < MOVEMAX ; ++c0){
+    if(m0[c0]){
+      for(int c1 = 0 ; c1 < MOVEMAX ; ++c1){
+        if(m1[c1]){
+          // if c0 is a shielded move, it's only relevant if c1 is a charged
+          // move, and vice versa
+          if(shielded_move_p(static_cast<pgo_move_e>(c0))){
+            if(!charged_move_p(static_cast<pgo_move_e>(c1))){
+              continue;
+            }
+          }
+          if(shielded_move_p(static_cast<pgo_move_e>(c1))){
+            if(!charged_move_p(static_cast<pgo_move_e>(c0))){
+              continue;
+            }
+          }
+          auto cs = std::make_unique<simulstate>();
+          *cs.get() = *s;
+          ++cs->turn;
+          //printf("calling bottomhalf with moves %d %d hp %d %d turn %u %u\n", c1, c2, cs->hp[0], cs->hp[1], s->turn, cs->turn);
+          bottomhalf(cs.get(), r, static_cast<pgo_move_e>(c0), static_cast<pgo_move_e>(c1));
+        }
+      }
+    }
+  }
+}
+
