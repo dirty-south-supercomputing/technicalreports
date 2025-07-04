374ca4a8e34c4594552f3adf645b1c14d8946dc3
nick black <dankamongmen@gmail.com>
2025-07-04 11:27:40 -0400

    break apart innercharged() and innerhalf()

--- 006-pgo/s/top.h
+++ 006-pgo/s/top.h
@@ -1,5 +1,5 @@
+// cartesian of player 1 with uncharged move of player 0
 static void innerhalf(const simulstate *s, results *r, pgo_move_e c0, unsigned m1mask){
-  bool c0charged = charged_move_p(c0);
   for(unsigned c1 = 1 ; c1 < MOVEMAX ; c1 <<= 1){
     if(m1mask & c1){ // valid move for c1; cartesian with c0
       bool c1charged = charged_move_p(static_cast<pgo_move_e>(c1));
@@ -9,26 +9,35 @@ static void innerhalf(const simulstate *s, results *r, pgo_move_e c0, unsigned m
         // need recurse in the case of two remaining.
         handle_ko(&cs, r); // pass the modified state
       }
-      if(c0charged && s->shields[1]){
-        cs = *s;
-        --cs.shields[1];
-        if(bottomhalf(&cs, r, static_cast<pgo_move_e>(c0), static_cast<pgo_move_e>(c1), false, true)){
-          handle_ko(&cs, r); // pass the modified state
-        }
-        if(c1charged && s->shields[0]){
-          cs = *s;
-          --cs.shields[0];
-          --cs.shields[1];
-          if(bottomhalf(&cs, r, static_cast<pgo_move_e>(c0), static_cast<pgo_move_e>(c1), true, true)){
-            handle_ko(&cs, r); // pass the modified state
-          }
-        }
-      }
       if(c1charged && s->shields[0]){
         cs = *s;
         --cs.shields[0];
         if(bottomhalf(&cs, r, static_cast<pgo_move_e>(c0), static_cast<pgo_move_e>(c1), true, false)){
           handle_ko(&cs, r); // pass the modified state
+        }
+      }
+    }
+  }
+}
+
+// cartesian of player 1 with a charged move of player 0
+static void innercharged(const simulstate *s, results *r, pgo_move_e c0, unsigned m1mask){
+  for(unsigned c1 = 1 ; c1 < MOVEMAX ; c1 <<= 1){
+    if(m1mask & c1){ // valid move for c1; cartesian with c0
+      bool c1charged = charged_move_p(static_cast<pgo_move_e>(c1));
+      if(s->shields[1]){
+        cs = *s;
+        --cs.shields[1];
+E       if(bottomhalf(&cs, r, static_cast<pgo_move_e>(c0), static_cast<pgo_move_e>(c1), false, true)){
+          handle_ko(&cs, r);
+        }
+        if(c1charged && s->shields[0]){
+          cs = *s;
+          --cs.shields[0];
+          --cs.shields[1];
+E         if(bottomhalf(&cs, r, static_cast<pgo_move_e>(c0), static_cast<pgo_move_e>(c1), true, true)){
+            handle_ko(&cs, r); // pass the modified state
+          }
         }
       }
     }
@@ -47,11 +56,11 @@ static void tophalf(const simulstate *s, results *r){
   }
   innerhalf(s, r, MOVE_FAST, m1mask);
   if(s->e[0][s->active[0]] >= -pmons[0][s->active[0]].ca1->energytrain){
-    innerhalf(s, r, MOVE_CHARGED1, m1mask);
+    innercharged(s, r, MOVE_CHARGED1, m1mask);
   }
   if(pmons[0][s->active[0]].ca2){
     if(s->e[0][s->active[0]] >= -pmons[0][s->active[0]].ca2->energytrain){
-      innerhalf(s, r, MOVE_CHARGED2, m1mask);
+      innercharged(s, r, MOVE_CHARGED2, m1mask);
     }
   }
   /*if(s->subtimer[0] == 0){
