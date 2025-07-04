d8c986aaef823b2c9a2fd2e858ee6a7ceee69904
nick black <dankamongmen@gmail.com>
2025-07-04 11:16:31 -0400

    implement faint check for substitution

--- 006-pgo/s/sift.h
+++ 006-pgo/s/sift.h
@@ -2,24 +2,26 @@
 static inline void sift_choices(const simulstate *s, unsigned *m, int player){
   *m = 0;
   if(s->turns[player]){ // if we're in a fast move, we can only wait
-    *m |= MOVE_WAIT; return;
-  }
-  // FIXME might want to do nothing if opp is in media fastmove
-  *m |= MOVE_FAST;
-  if(s->e[player][s->active[player]] >=
-      -pmons[player][s->active[player]].ca1->energytrain){
-    *m |= MOVE_CHARGED1;
-  }
-  if(pmons[player][s->active[player]].ca2){
+    *m |= MOVE_WAIT;
+  }else{ // FIXME might want to do nothing if opp is in media fastmove
+    *m |= MOVE_FAST;
     if(s->e[player][s->active[player]] >=
-        -pmons[player][s->active[player]].ca2->energytrain){
-      *m |= MOVE_CHARGED2;
+        -pmons[player][s->active[player]].ca1->energytrain){
+      *m |= MOVE_CHARGED1;
+    }
+    if(pmons[player][s->active[player]].ca2){
+      if(s->e[player][s->active[player]] >=
+          -pmons[player][s->active[player]].ca2->energytrain){
+        *m |= MOVE_CHARGED2;
+      }
+    }
+    if(s->subtimer[player] == 0){
+      if(s->hp[player][(s->active + 1) % 3]){
+        *m |= MOVE_SUB1;
+      }
+      if(s->hp[player][(s->active + 2) % 3]){
+        *m |= MOVE_SUB2;
+      }
     }
   }
-  if(s->subtimer[player] == 0){
-    // FIXME need check that they've not fainted
-    *m |= MOVE_SUB1;
-    *m |= MOVE_SUB2;
-  }
 }
-
