df8685df0c66a5d60fda307e0e5931125973fdab
nick black <dankamongmen@gmail.com>
2025-07-06 04:00:53 -0400

    darken electric color

--- 006-pgo/breakpoints.cpp
+++ 006-pgo/breakpoints.cpp
@@ -85,7 +85,7 @@ lex_pmon(pmon* p, int *hp, int *argc, char ***argv){
 
 static void
 print_dbreak_table(pmon *p, pmon *atk, const attack *a, int tableno){
-  printf("\\begin{table}\\setlength{\\tabcolsep}{6pt}\\footnotesize\\centering\\begin{tabular}{rcccccccccccccccc}\n");
+  printf("\\begin{table}\\setlength{\\tabcolsep}{2pt}\\footnotesize\\centering\\begin{tabular}{rcccccccccccccccc}\n");
   printf("$IV_D$ & 0 & 1 & 2 & 3 & 4 & 5 & 6 & 7 & 8 & 9 & 10 & 11 & 12 & 13 & 14 & 15\\\\\n");
   printf("\\Midrule\n");
   int firstd = -1;
