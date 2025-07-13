4babe97b312555b964804571b3d7ff1585532463
nick black <dankamongmen@gmail.com>
2025-07-13 01:08:08 -0400

    80 chars

--- 006-pgo/s/bottom.h
+++ 006-pgo/s/bottom.h
@@ -8,10 +8,10 @@ static bool account_fast_move(simulstate *s, int player){
       accumulate_energy(&s->e[player][s->active[player]], p->fa->energytrain);
       int op = other_player(player);
       return inflict_damage(&s->hp[op][s->active[op]],
-              calc_buffed_damage(
-                calc_damage(&pmons[player][s->active[player]], &pmons[op][s->active[op]],
-                            pmons[player][s->active[player]].fa),
-                s->buffleva[player], s->bufflevd[op]));
+              calc_buffed_damage(calc_damage(&pmons[player][s->active[player]],
+                                  &pmons[op][s->active[op]],
+                                  pmons[player][s->active[player]].fa),
+                                 s->buffleva[player], s->bufflevd[op]));
     }
   }
   return false;
@@ -29,7 +29,8 @@ static int p0_wins_cmp(const simulstate *s){
 // out own simulstate in which we can scribble. corecurses back into tophalf().
 // return true if anyone was ko'd.
 static inline bool
-bottomhalf(simulstate *s, results *r, pgo_move_e m0, pgo_move_e m1, bool m0shield, bool m1shield){
+bottomhalf(simulstate *s, results *r, pgo_move_e m0, pgo_move_e m1,
+            bool m0shield, bool m1shield){
   ++r->nodes;
   if(sub_move_p(m0) || sub_move_p(m1)){
     return false;
