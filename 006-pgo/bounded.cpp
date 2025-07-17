70df54aac96c88c43d543e09a61922b3e083ae55
M-Alshamsi <119577197+M-Alshamsi@users.noreply.github.com>
2025-07-17 04:16:28 +0400

    wrapped all "Eff_[ADS]" occurrences in \mathit
    I think the space between the letters here hurts the look too much
    as it is now. Another really bad example is MHP_{New} in pokemon.tex,
    or page 97. It's probably worth the effort to go back and put \mathit
    everywhere.
      Eff_[ADS] was thankfully simple with sed but other replacements will
    probably need manual effort.

--- 006-pgo/bounded.cpp
+++ 006-pgo/bounded.cpp
@@ -63,7 +63,7 @@ void print_bounded_table(int bound, float lbound, bool amean){
   printf("\\footnotesize\n");
   printf("\\setlength{\\tabcolsep}{1pt}\n");
   printf("\\begin{longtable}{lrrrrrrrrr}\n");
-  printf("Species & L & IVs & HP & $Eff_A$ & $Eff_D$ & $\\frac{BS}{3}$ & $\\sqrt[3]{BP}$ & CP & A\\%% \\\\\n");
+  printf("Species & L & IVs & HP & $\\mathit{Eff_A}$ & $\\mathit{Eff_D}$ & $\\frac{BS}{3}$ & $\\sqrt[3]{BP}$ & CP & A\\%% \\\\\n");
   printf("\\Midrule\n");
   printf("\\endhead\n");
   stats *sols = NULL;
