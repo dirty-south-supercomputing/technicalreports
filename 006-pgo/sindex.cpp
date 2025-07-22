779712d1ea9e1aa796d50cdf019ea457a8e2f63c
nick black <dankamongmen@gmail.com>
2025-07-22 14:05:48 -0400

    remove unused function scmp

--- 006-pgo/sindex.cpp
+++ 006-pgo/sindex.cpp
@@ -3,13 +3,6 @@
 #include <iostream>
 
 static int
-scmp(const void *va1, const void *va2){
-  const species *s1 = static_cast<const species*>(va1);
-  const species *s2 = static_cast<const species*>(va2);
-  return strcmp(s1->name.c_str(), s2->name.c_str());
-}
-
-static int
 index_sdex(const species *dex, unsigned dcount, int cols){
   int total = 0;
   for(unsigned i = 0 ; i < dcount ; ++i){
