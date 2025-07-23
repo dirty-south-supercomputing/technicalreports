f5641ff7261f71dd14296f6c1fd39a33b11cb2cf
nick black <dankamongmen@gmail.com>
2025-07-23 02:37:06 -0400

    tighten pmon, don't use typedef in c++

--- 006-pgo/s/simul.h
+++ 006-pgo/s/simul.h
@@ -1,33 +1,29 @@
 constexpr unsigned TEAMSIZE = 3;
 constexpr unsigned ENERGY_MAX = 100;
 
-typedef struct results {
+struct results {
   unsigned long wins[2], ties; // win/tie counts
-} results;
+};
 
-using hpoints = uint16_t;
-using energy = uint16_t;
-
-typedef struct simulstate { // dynamic elements
-  hpoints hp[2][TEAMSIZE];  // hp of pokemon
+struct simulstate { // dynamic elements
+  uint16_t hp[2][TEAMSIZE]; // hp of pokemon
+  uint16_t e[2][TEAMSIZE];  // energy of pokemon         [0..100]
+  uint16_t dam[2][TEAMSIZE];// precalc damage for attacks
   uint8_t turns[2];         // turns left in fast attack   [0..5]
-  energy e[2][TEAMSIZE];    // energy of pokemon         [0..100]
   uint8_t subtimer[2];      // substitution timers       [0..100]
   int8_t buffleva[2];       // active attack buff levels  [-4..4]
   int8_t bufflevd[2];       // active defense buff levels [-4..4]
   uint8_t shields[2];       // number of shields available [0..2]
   uint8_t active[2];        // indexes of active pokémon   [0..2]
-  uint16_t dam[2][TEAMSIZE];// precalc damage for attacks
   int cmp;                  // <=> for charged move prio
-} simulstate;
+};
 
-typedef struct pmon { // static elements
+struct pmon { // static elements
   struct stats s;
   const attack *fa, *ca1, *ca2;
-  float effa;  // effective attack, cpm * (atk + iva)
-  float effd;  // effective defense, cpm * (def + ivd)
+  float effa, effd; // effective stats, cpm * (base + iv)
   bool shadow;
-} pmon;
+};
 
 extern pmon pmons[2][TEAMSIZE];
 
