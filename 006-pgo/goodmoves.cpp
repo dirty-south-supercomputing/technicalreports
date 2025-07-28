c548c2d956f428b56084c4bf87cd30f23e90eeff
nick black <dankamongmen@gmail.com>
2025-07-28 14:20:54 -0400

    use newunicodechar for heart symbol

--- 006-pgo/goodmoves.cpp
+++ 006-pgo/goodmoves.cpp
@@ -3,22 +3,20 @@
 // show fast moves according to some condition
 // for now, condition is fixed: ept > 3 and ppt > 3
 int main(void){
-  constexpr int eptfloor = 4;
-  constexpr int pptfloor = 4;
+  constexpr int eptfloor = 3;
+  constexpr int pptfloor = 3;
   for(const attack * const *as = attacks ; *as ; ++as){
     const attack *a = *as;
     if(a->energytrain <= 0){
       continue;
     }
-    if(a->energytrain / static_cast<float>(a->turns) <= eptfloor){
-      printf("%s fails ept test\n", a->name);
+    if(a->energytrain / static_cast<float>(a->turns) < eptfloor){
       continue;
     }
-    if(a->powertrain / static_cast<float>(a->turns) <= pptfloor){
-      printf("%s fails ppt test\n", a->name);
+    if(a->powertrain / static_cast<float>(a->turns) < pptfloor){
       continue;
     }
-    printf("%s\n", a->name);
+    printf("%s %u %u %u\n", a->name, a->powertrain, a->energytrain, a->turns);
   }
   return EXIT_SUCCESS;
 }
