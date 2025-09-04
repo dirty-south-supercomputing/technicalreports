2002741480274dcfb0fab8cc68c677d652d23040
nick black <dankamongmen@gmail.com>
2025-09-04 00:55:52 -0400

    use 4 columns for unpopulated typings table

--- 006-pgo/unpop.cpp
+++ 006-pgo/unpop.cpp
@@ -1,10 +1,12 @@
 #include "pgotypes.cpp"
 
-// list all unpopulated typings
+// list all unpopulated typings. if the number changes, we'll probably want a different
+// number of columns, so throw an exception if they're not full.
 int main(void){
-  std::cout << "\\begin{table}[ht]\\centering\\begin{tabular}{c|c|c}" << std::endl;
+  std::cout << "\\begin{table}[ht]\\centering\\begin{tabular}{c|c|c|c}" << std::endl;
   int count = 0;
   int column = 0;
+  constexpr int COLUMNS = 4;
   for(int i = 0 ; i < TYPECOUNT ; ++i){
     for(int j = i ; j < TYPECOUNT ; ++j){
       bool found = false;
@@ -17,7 +19,7 @@ int main(void){
       if(!found){
         std::cout << TNames[i] << "/" << TNames[j];
         ++count;
-        if(++column == 3){
+        if(++column == COLUMNS){
           std::cout << "\\\\" << std::endl;
           column = 0;
         }else{
@@ -27,10 +29,12 @@ int main(void){
     }
   }
   if(column){
-    if(column == 1){
+    std::cerr << "column didn't equal 0 following table!" << std::endl;
+    return EXIT_FAILURE;
+    /* if(column == 1){
       std::cout << " & ";
     }
-    std::cout << "\\\\" << std::endl;
+    std::cout << "\\\\" << std::endl;*/
   }
   std::cout << "\\end{tabular}\\caption{The " << count << " unpopulated typings\\label{table:unpopulated}}\\end{table}" << std::endl;
   std::cout << count << " typings are currently unpopulated (\\autoref{table:unpopulated}), ";
