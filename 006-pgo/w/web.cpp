7ea8238961a2915f37d45294fa0d05a606892874
nick black <dankamongmen@gmail.com>
2025-09-05 16:14:38 -0400

    run webbin from makefile

--- 006-pgo/w/web.cpp
+++ 006-pgo/w/web.cpp
@@ -1,5 +1,20 @@
 #include "../pgotypes.cpp"
+#include <fcntl.h>
 
-int main(void){
+static void
+usage(const char *argv0){
+  std::cerr << "usage: " << argv0 << " outdir" << std::endl;
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, const char **argv){
+  const char *argv0 = *argv;
+  if(argc != 2){
+    usage(argv0);
+  }
+  int dfd = open(argv[1], O_DIRECTORY);
+  if(dfd < 0){
+    std::cerr << "error opening " << argv[1] << " (" << strerror(errno) << ")" << std::endl;
+  }
   return EXIT_SUCCESS;
 }
