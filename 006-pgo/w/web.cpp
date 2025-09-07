70d906a47aadb74ca6af162e9e6aa4db02e5a307
nick black <dankamongmen@gmail.com>
2025-09-07 17:19:37 -0400

    w

--- 006-pgo/w/web.cpp
+++ 006-pgo/w/web.cpp
@@ -1,5 +1,37 @@
 #include "../pgotypes.cpp"
 #include <fcntl.h>
+#include <unistd.h>
+
+static int
+open_output(int dfd, const char *node){
+  int indexfd = openat(dfd, node, O_CREAT);
+  if(indexfd < 0){
+    std::cerr << "error opening " << node << " (" << strerror(errno) << ")" << std::endl;
+    return -1;
+  }
+  return indexfd;
+}
+
+static int
+close_output(int fd){
+  if(close(fd)){
+    std::cerr << "error closing " << fd << " (" << strerror(errno) << ")" << std::endl;
+    return -1;
+  }
+  return 0;
+}
+
+static int
+write_page(int dfd){
+  int indexfd = open_output(dfd, "index.html");
+  if(indexfd < 0){
+    return -1;
+  }
+  if(close_output(indexfd)){
+    return -1;
+  }
+  return 0;
+}
 
 static void
 usage(const char *argv0){
@@ -15,6 +47,12 @@ int main(int argc, const char **argv){
   int dfd = open(argv[1], O_DIRECTORY);
   if(dfd < 0){
     std::cerr << "error opening " << argv[1] << " (" << strerror(errno) << ")" << std::endl;
+    usage(argv0);
   }
-  return EXIT_SUCCESS;
+  int r = write_page(dfd);
+  if(close(dfd)){
+    std::cerr << "error opening " << argv[1] << " (" << strerror(errno) << ")" << std::endl;
+    return EXIT_FAILURE;
+  }
+  return r ? EXIT_FAILURE : EXIT_SUCCESS;
 }
