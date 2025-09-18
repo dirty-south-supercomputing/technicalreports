1b474c9ee485b1de49a2bfbdd97e40051d8bbe58
nick black <dankamongmen@gmail.com>
2025-09-18 02:37:13 -0400

    list first partners in regions table programmatically

--- 006-pgo/regions.cpp
+++ 006-pgo/regions.cpp
@@ -1,5 +1,35 @@
 #include "pgotypes.h"
 
+static void
+print_first_partners(int region){
+    // FIXME print first partners in smaller font
+  // first, determine that there *are* first partners from this region
+  unsigned lastpdex = 0;
+  bool firstfound = false;
+  for(int i = 0 ; i < SPECIESCOUNT ; ++i){
+    const auto s = &sdex[i];
+    if(s->idx == lastpdex){ // skip alternate forms
+      continue;
+    }
+    lastpdex = s->idx;
+    const int gen = idx_to_region_int(s->idx);
+    if(gen != region){
+      continue;
+    }
+    if(s->category != species::CAT_FPARTNER){
+      continue;
+    }
+    if(!firstfound){
+      printf("\\multicolumn{4}{r}{\\footnotesize{}First partners: ");
+      firstfound = true;
+    }
+    printf("%s ", s->name.c_str());
+  }
+  if(firstfound){
+    printf("}\\\\");
+  }
+}
+
 int main(void){
   int regpop[REGION_COUNT] = {};
   unsigned lastpdex = 0;
@@ -16,14 +46,15 @@ int main(void){
     }
     ++regpop[gen];
   }
-  printf("\\begin{table}\\centering\\begin{tabular}{lcrrr}");
-  printf("Region & Generation & Start & End & Pop\\\\\\Midrule\n");
+  printf("\\begin{table}\\centering\\begin{tabular}{lcrr}");
+  printf("Region & Gen & Range & Pop\\\\\\Midrule\n");
   for(int i = 0 ; i < REGION_COUNT ; ++i){
     int first = region_idx_first(i);
     int last = region_idx_last(i);
-    printf("%s & %s & \\#%04d & \\#%04d & %d/%d\\\\\n",
+    printf("%s & %s & \\#%04d--%04d & %d/%d\\\\\n",
         idx_to_region(first), idx_to_generation(first),
         first, last, regpop[i], last - first + 1);
+    print_first_partners(i);
   }
   printf("\\end{tabular}\\caption[Regions of the Pokémon world]{Regions of the Pokémon world (\\textjapanese{ポケモンの世界}). Populations do not count multiple forms.\\label{table:regions}}");
   printf("\\end{table}");
