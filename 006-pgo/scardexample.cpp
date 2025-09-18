123a7e06ef05a4af7416ace334ba2915111b8c8e
nick black <dankamongmen@gmail.com>
2025-09-18 05:54:08 -0400

    don't color species card example page

--- 006-pgo/scardexample.cpp
+++ 006-pgo/scardexample.cpp
@@ -6,5 +6,5 @@ int main(void){
                   { &ATK_Astonish, &ATK_Incinerate, &ATK_Brave_Bird, &ATK_Aeroblast, },
                   true, true, false, { &ATK_Aeroblast, }, species::CAT_NORMAL,
                   100, nullptr, species::EVOL_NOITEM);
-  print_species_latex(&s, true, true, true);
+  print_species_latex(&s, true, false, true);
 }
