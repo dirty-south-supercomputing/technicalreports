7bc8bcee19356faf4d86dfd65bee9dcc0703042d
nick black <dankamongmen@gmail.com>
2025-10-20 13:18:26 -0400

    build level item tables from code

--- 006-pgo/xp.cpp
+++ 006-pgo/xp.cpp
@@ -8,8 +8,58 @@ static void emitk(uint64_t val){
   }
 }
 
+static void level_item_table(int llevel, int hlevel){
+  std::cout << "\\begin{table}";
+  std::cout << "\\centering";
+  std::cout << "\\setlength{\\tabcolsep}{1pt}";
+  std::cout << "\\footnotesize";
+  std::cout << "\\begin{tabular}{r|g c g c g c g c g c g c g c g c g c g c g c g}";
+  std::cout << "&\
+\\includegraphics[width=1em]{images/pokeball.png} &\
+\\includegraphics[width=1em]{images/greatball.png} &\
+\\includegraphics[width=1em]{images/ultraball.png} &\
+\\includegraphics[width=1em]{images/nanab.png} &\
+\\includegraphics[width=1em]{images/razz.png} &\
+\\includegraphics[width=1em]{images/pinap.png} &\
+\\includegraphics[width=1em]{images/silverpinap.png} &\
+\\includegraphics[width=1em]{images/incense.png} &\
+\\includegraphics[width=1em]{images/revive.png} &\
+\\includegraphics[width=1em]{images/maxrevive.png} &\
+\\includegraphics[width=1em]{images/Potion.png} &\
+\\includegraphics[width=1em]{images/superpotion.png} &\
+\\includegraphics[width=1em]{images/Hyper_Potion.png} &\
+\\includegraphics[width=1em]{images/Max_Potion.png} &\
+\\includegraphics[width=1em]{images/lure.png} &\
+\\includegraphics[width=1em]{images/luckyegg.png} &\
+\\includegraphics[width=1em]{images/incubatorlimited.png} &\
+\\includegraphics[width=1em]{images/incubatorsuper.png} &\
+\\includegraphics[width=1em]{images/rarecandyxl.png} &\
+\\includegraphics[width=1em]{images/elitefasttm.png} &\
+\\includegraphics[width=1em]{images/elitechargedtm.png}";
+  std::cout << "\\end{tabular}";
+  std::cout << "\\caption{Items awarded for reaching Trainer Levels ";
+  std::cout << llevel << "--" << hlevel;
+  std::cout << "\\label{table:level" << hlevel << "items}}";
+  std::cout << "\\end{table}";
+}
+
 // write the trainer XP table
-int main(void){
+int main(int argc, const char **argv){
+  if(argc > 1){
+    if(argc != 3){
+      std::cerr << "usage: " << argv[0] << " [ llevel hlevel ]" << std::endl;
+      return EXIT_FAILURE;
+    }
+    int llevel = atoi(argv[1]);
+    int hlevel = atoi(argv[2]);
+    if(llevel <= 0 || hlevel <= 0 || llevel > MAXLEVEL || hlevel > MAXLEVEL || llevel > hlevel){
+      std::cerr << "usage: " << argv[0] << " [ llevel hlevel ]" << std::endl;
+      return EXIT_FAILURE;
+    }
+    std::cout << "ll: " << llevel << " hl: " << hlevel << std::endl;
+    level_item_table(llevel, hlevel);
+    return EXIT_SUCCESS;
+  }
   std::cout << "\\begin{table}";
   std::cout << "\\centering\\footnotesize";
   std::cout << "\\setlength{\\tabcolsep}{2pt}";
