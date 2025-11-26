5c5d777588e98b7a4d62bfab5dbc1d3a95b2f305
nick black <dankamongmen@gmail.com>
2025-11-26 09:53:12 -0500

    energy changes for precious paths

--- 006-pgo/pgotypes.h
+++ 006-pgo/pgotypes.h
@@ -695,7 +695,8 @@ static const attack ATK_Megahorn = { "Megahorn", TYPE_BUG, 110, -55, 0, 0, 0, 0,
 	105, 100, 4, false, };
 static const attack ATK_Meteor_Beam = { "Meteor Beam", TYPE_ROCK, 120, -60, 0, 1000, 0, 0, 0, 1, 0, 0, 0,
 	140, 100, 4, false, };
-static const attack ATK_Meteor_Mash = { "Meteor Mash", TYPE_STEEL, 100, -50, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// FIXME verify for precious paths
+static const attack ATK_Meteor_Mash = { "Meteor Mash", TYPE_STEEL, 100, -45, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	100, 50, 5, false, };
 static const attack ATK_Mirror_Coat = { "Mirror Coat", TYPE_PSYCHIC, 60, -55, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	60, 50, 5, false, };
@@ -733,7 +734,8 @@ static const attack ATK_Outrage = { "Outrage", TYPE_DRAGON, 110, -60, 0, 0, 0, 0
 	110, 50, 8, false, };
 static const attack ATK_Overheat = { "Overheat", TYPE_FIRE, 130, -55, 0, 1000, 0, 0, 0, -2, 0, 0, 0,
 	160, 100, 8, false, };
-static const attack ATK_Parabolic_Charge = { "Parabolic Charge", TYPE_ELECTRIC, 70, -50, 0, 0, 660, 0, 0, 0, 1, 0, 0,
+// FIXME verify for precious paths
+static const attack ATK_Parabolic_Charge = { "Parabolic Charge", TYPE_ELECTRIC, 70, -55, 0, 0, 660, 0, 0, 0, 1, 0, 0,
 	70, 50, 6, false, };
 static const attack ATK_Payback = { "Payback", TYPE_DARK, 110, -60, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	95, 100, 4, false, };
@@ -802,7 +804,8 @@ static const attack ATK_Seed_Bomb = { "Seed Bomb", TYPE_GRASS, 55, -40, 0, 0, 0,
 	55, 33, 4, false, };
 static const attack ATK_Seed_Flare = { "Seed Flare", TYPE_GRASS, 130, -75, 0, 0, 0, 0, 400, 0, 0, 0, -2,
 	115, 100, 5, false, };
-static const attack ATK_Shadow_Ball = { "Shadow Ball", TYPE_GHOST, 100, -55, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// FIXME verify for precious paths
+static const attack ATK_Shadow_Ball = { "Shadow Ball", TYPE_GHOST, 100, -50, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	100, 50, 6, false, };
 static const attack ATK_Shadow_Bone = { "Shadow Bone", TYPE_GHOST, 80, -45, 0, 0, 0, 0, 200, 0, 0, 0, -1,
 	85, 50, 6, false, };
@@ -810,7 +813,8 @@ static const attack ATK_Shadow_Force = { "Shadow Force", TYPE_GHOST, 120, -90, 0
 	140, 100, -4, false, };
 static const attack ATK_Shadow_Punch = { "Shadow Punch", TYPE_GHOST, 55, -35, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	35, 33, 3, false, };
-static const attack ATK_Shadow_Sneak = { "Shadow Sneak", TYPE_GHOST, 50, -45, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// FIXME verify for precious paths
+static const attack ATK_Shadow_Sneak = { "Shadow Sneak", TYPE_GHOST, 50, -50, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	50, 33, 6, false, };
 static const attack ATK_Signal_Beam = { "Signal Beam", TYPE_BUG, 75, -55, 0, 0, 0, 200, 200, 0, 0, -1, -1,
 	75, 50, 6, false, };
@@ -828,7 +832,7 @@ static const attack ATK_Sludge_Wave = { "Sludge Wave", TYPE_POISON, 110, -65, 0,
 	105, 100, 6, false, };
 static const attack ATK_Solar_Beam = { "Solar Beam", TYPE_GRASS, 150, -80, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	180, 100, 10, false, };
-static const attack ATK_Spacial_Rend = { "Spacial Rend", TYPE_DRAGON, 95, -50, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+static const attack ATK_Spacial_Rend = { "Spacial Rend", TYPE_DRAGON, 95, -55, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	160, 100, 5, true, };
 static const attack ATK_Sparkling_Aria = { "Sparkling Aria", TYPE_WATER, 80, -45, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	85, 33, 6, false, };
