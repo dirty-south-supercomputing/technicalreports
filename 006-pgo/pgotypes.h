6ca2ed4f0fd4d6e5251e509cced935e8502cef45
nick black <dankamongmen@gmail.com>
2026-01-15 17:22:24 -0500

    add glimmet and glimmora

--- 006-pgo/pgotypes.h
+++ 006-pgo/pgotypes.h
@@ -5045,8 +5045,12 @@ static const species sdex[] = {
 		false, false, false, { }, species::CAT_NORMAL, 50, nullptr, species::EVOL_NOITEM, species::REGION_ALL, },
   // 967 cyclizar
   // 968 orthworm
-  // 969 glimmet
-  // 970 glimmora
+  {  969, "Glimmet", TYPE_ROCK, TYPE_POISON, 187, 104, 134, nullptr,
+		{ &ATK_Rock_Throw, &ATK_Smack_Down, &ATK_Power_Gem, &ATK_Dazzling_Gleam, &ATK_Sludge_Wave, },
+		false, false, false, { }, species::CAT_NORMAL, 50, nullptr, species::EVOL_NOITEM, species::REGION_ALL, },
+  {  970, "Glimmora", TYPE_ROCK, TYPE_POISON, 246, 177, 195, "Glimmet",
+		{ &ATK_Rock_Throw, &ATK_Smack_Down, &ATK_Power_Gem, &ATK_Dazzling_Gleam, &ATK_Sludge_Wave, },
+		false, false, false, { }, species::CAT_NORMAL, 50, nullptr, species::EVOL_NOITEM, species::REGION_ALL, },
   {  971, "Greavard", TYPE_GHOST, TYPECOUNT, 105, 106, 137, nullptr,
 		{ &ATK_Bite, &ATK_Lick, &ATK_Dig, &ATK_Shadow_Ball, &ATK_Psychic_Fangs, },
 		false, false, false, { }, species::CAT_NORMAL, 50, nullptr, species::EVOL_NOITEM, species::REGION_ALL, },
