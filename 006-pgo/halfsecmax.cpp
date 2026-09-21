69809a23c4dcd8ddb623fd877d52b4dec02eda34
nick black <dankamongmen@gmail.com>
2026-09-21 02:05:21 -0400

    halfsecmax: special case zamian, zamazenta, and eternatus

--- 006-pgo/halfsecmax.cpp
+++ 006-pgo/halfsecmax.cpp
@@ -4,9 +4,14 @@
 int main(void){
   for(unsigned u = 0 ; u < SPECIESCOUNT ; ++u){
     const auto* s = &sdex[u];
-    if(!has_dmax(s) && !has_gmax(s)){
-      continue;
+    if(s->name != "Zacian Crowned Sword"
+        && s->name != "Zamazenta Crowned Shield"
+        && s->name != "Eternatus"){
+      if(!has_dmax(s) && !has_gmax(s)){
+        continue;
+      }
     }
+    bool printed = false;
     for(const auto* a : s->attacks){
       if(!fast_attack_p(a)){
         continue;
@@ -14,7 +19,15 @@ int main(void){
       if(a->animdur > 1){
         continue;
       }
-      std::cout << s->name << " (" << a->name << ")" << std::endl;
+      if(printed){
+        std::cout << ", " << a->name;
+      }else{
+        std::cout << s->name << " (" << a->name;
+        printed = true;
+      }
+    }
+    if(printed){
+      std::cout << ")" << std::endl;
     }
   }
   return EXIT_SUCCESS;
